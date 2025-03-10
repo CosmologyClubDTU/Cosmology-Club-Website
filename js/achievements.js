16dc1dc791ea665c4350d1491b212af56922e84b
CosmologyClubDTU <cosmologyclubdtu@gmail.com>
2025-03-10 13:55:34 +0000

    Revert "Updated Hamburger Icon on all pages except Team"
    This reverts commit 633b2c80afc013ad1c0745490a5381b7af7fd265.

--- js/achievements.js
+++ js/achievements.js
@@ -10,21 +10,21 @@ addEventListener("click", (e) => {
     _MAIN.style.setProperty("--k", (k = (k + 1) % N));
 });
 
-// const hamburger_menu = document.querySelector(".hamburger-menu");
-// const container = document.querySelector(".container");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
 
-// hamburger_menu.addEventListener("click", function() {
-//     container.classList.toggle("active");
-// })
+hamburger_menu.addEventListener("click", function() {
+    container.classList.toggle("active");
+})
 
-// const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 
-// hamburger_menu.addEventListener("click", function() {
-//     navbar.classList.toggle("active");
-// })
+hamburger_menu.addEventListener("click", function() {
+    navbar.classList.toggle("active");
+})
 
-// const main = document.querySelector(".achievements");
-// main.addEventListener("click", function() {
-//     container.classList.toggle("active");
-//     navbar.classList.toggle("active");
-// })
+const main = document.querySelector(".achievements");
+main.addEventListener("click", function() {
+    container.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
